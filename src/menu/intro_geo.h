51c2bc965f72da6c508f0811a3ef454bbd79edda
MaikelChan <maikelchan88@gmail.com>
2021-01-09 00:10:24 +0100

    Merged the framebuffer effects of the title screen from the Shindou edition.

--- src/menu/intro_geo.h
+++ src/menu/intro_geo.h
@@ -5,11 +5,14 @@
 #include <PR/gbi.h>
 
 #include "types.h"
+#include "engine/graph_node.h"
 
 Gfx *geo_title_screen(s32 sp50, struct GraphNode *sp54, UNUSED void *context);
 Gfx *geo_fade_transition(s32 sp40, struct GraphNode *sp44, UNUSED void *context);
 Gfx *intro_backdrop_one_image(s32 index, s8 *backgroundTable);
 Gfx *geo_intro_backdrop(s32 sp48, struct GraphNode *sp4c, UNUSED void *context);
 Gfx *geo_game_over_tile(s32 sp40, struct GraphNode *sp44, UNUSED void *context);
+
+Gfx *geo_intro_face_easter_egg(s32 state, struct GraphNode *node, UNUSED void *context);
 
 #endif // INTRO_GEO_H
